42f62998b1c9c0dcdd664678006ac2a2c0054029
LeonardoBrizolla <leonardobrizollars@gmail.com>
2022-01-12 14:29:19 -0300

    Created card only jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Card } from './objects/Card';
 import Header from './components/Header';
 
 const App = () => {
   return (
     <>
       <Header />
+      <Card />
     </>
   );
 };
---
+++ src/objects/Card/index.js
@@ -0,0 +1,3 @@
+export const Card = () => {
+  return <article className="card">Card</article>;
+};
