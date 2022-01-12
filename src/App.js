025a2bcc6bc3841bbf97c403556a715b6db6038a
LeonardoBrizolla <leonardobrizollars@gmail.com>
2022-01-12 00:46:16 -0300

    CSS do object do menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Logo from './objects/Logo';
 import imgLogo from './img/logo.svg';
 import Sobre from './objects/Sobre';
+import Menu from './objects/Menu';
 
 const App = () => {
   return (
     <>
       <Logo srcImage={imgLogo} textAlt="Logo CollabCode" />
       <Sobre content="Sobre" />
+      <Menu />
     </>
   );
 };
---
+++ src/objects/Menu/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+const Menu = () => {
+  return (
+    <a className="menu" href="#to-do">
+      <span className="center">Menu</span>
+    </a>
+  );
+};
+
+export default Menu;
