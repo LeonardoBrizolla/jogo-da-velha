3f91a111263032102c9fe51838b75857f609dbb6
LeonardoBrizolla <leonardobrizollars@gmail.com>
2022-01-12 14:59:32 -0300

    Created css object player X

--- src/objects/Player/index.js
+++ src/objects/Player/index.js
@@ -1,5 +1,10 @@
 import './style.css';
+import playerXSVG from '../../img/player-x.svg';
 
 export const Player = () => {
-  return <button className="btn-player">X</button>;
+  return (
+    <button className="btn-player">
+      <img src={playerXSVG} alt="Jogador X" />
+    </button>
+  );
 };
