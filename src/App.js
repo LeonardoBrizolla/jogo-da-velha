e43a08d637609a1c4c37e8668fb981e58e910a2c
leonardobrizolla <leonardobrizollars@gmail.com>
2022-01-13 00:01:46 -0300

    Created object Label JSX/CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Header } from './components/Header';
 import { Hashtag } from './components/Hashtag';
+import { Label } from './objects/Label';
 
 const App = () => (
   <main className="app">
     <Header />
     <Hashtag />
+    <Label content="Mostrar eventos" />
   </main>
 );
 
---
+++ src/objects/Label/index.js
@@ -0,0 +1,5 @@
+import './style.css';
+
+export const Label = ({ content }) => (
+  <label className="label">{content}</label>
+);
