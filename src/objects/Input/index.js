8790c1f5a9a278a5cf25cf1907754961b40d27ac
leonardobrizolla <leonardobrizollars@gmail.com>
2022-01-13 11:56:54 -0300

    CSS Checkbox  parte 1 without check

--- src/objects/Input/index.js
+++ src/objects/Input/index.js
@@ -1,3 +1,3 @@
 export const Input = ({ id = '', value = '', type = 'text' }) => (
-  <input id={id} type={type} value={value} />
+  <input className="input" id={id} type={type} value={value} />
 );
