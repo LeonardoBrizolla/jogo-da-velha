68ce23830c26201567cd49fd52fb8479ed9d9302
leonardobrizolla <leonardobrizollars@gmail.com>
2022-01-14 09:25:08 -0300

    Created IconSocial Object JSX

--- src/components/ProfileUser/index.js
+++ src/components/ProfileUser/index.js
@@ -1,7 +1,13 @@
 import './styles.css';
 import AvatarProfileJPEG from '../../img/avatar-profile.jpeg';
+import GithubSVG from '../../img/github.svg';
+import LinkedInSVG from '../../img/linkedin.svg';
+import TwitterSVG from '../../img/twitter.svg';
+import FacebookSVG from '../../img/facebook.svg';
+import InstagramSVG from '../../img/instagram.svg';
 
 import { AvatarProfile } from '../../objects/AvatarProfile';
+import { IconSocial } from '../../objects/IconSocial';
 
 export const ProfileUser = () => (
   <dl className="profile-user">
@@ -13,5 +19,12 @@ export const ProfileUser = () => (
     </dd>
     <dt className="title">Leonardo Brizolla</dt>
     <dd className="description">Desenvolvedor Web</dd>
+    <dd>
+      <IconSocial src={GithubSVG} alt="Logo da Github" />
+      <IconSocial src={LinkedInSVG} alt="Logo da LinkedIn" />
+      <IconSocial src={TwitterSVG} alt="Logo da Twitter" />
+      <IconSocial src={FacebookSVG} alt="Logo da Facebook" />
+      <IconSocial src={InstagramSVG} alt="Logo da Instagram" />
+    </dd>
   </dl>
 );
---
+++ src/objects/IconSocial/index.js
@@ -0,0 +1,3 @@
+export const IconSocial = ({ src, alt }) => (
+  <img className="icon-social" src={src} alt={alt} />
+);
