2cc5fde2c80c402b59d86519011b1d5e6463debe
leonardobrizolla <leonardobrizollars@gmail.com>
2022-01-14 10:23:00 -0300

    Created Link and Menu handleClick console

--- src/objects/Link/index.js
+++ src/objects/Link/index.js
@@ -1,7 +1,9 @@
 import './styles.css';
 
+const handleClick = () => console.log('click sobre');
+
 export const Link = ({ content = '', className = '' }) => (
-  <a href="#to-do" className={`link ${className}`}>
+  <a href="#to-do" className={`link ${className}`} onClick={handleClick}>
     {content}
   </a>
 );
--- src/objects/Menu/index.js
+++ src/objects/Menu/index.js
@@ -1,7 +1,9 @@
 import './styles.css';
 
+const handleClick = () => console.log('click menu');
+
 export const Menu = () => (
-  <a href="#to-do" className="menu">
+  <a href="#to-do" className="menu" onClick={handleClick}>
     <span className="center">Menu</span>
   </a>
 );
