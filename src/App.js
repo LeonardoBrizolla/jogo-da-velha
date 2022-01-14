f7cc703e70489b8ed1973cfc9f384f7efc394946
leonardobrizolla <leonardobrizollars@gmail.com>
2022-01-13 21:41:16 -0300

    Created About object JSX

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 
+import { About } from './objects/About';
 import { Checkbox } from './objects/Checkbox';
 import { Header } from './components/Header';
 import { Hashtag } from './components/Hashtag';
@@ -9,6 +10,7 @@ const App = () => (
     <Header />
     <Hashtag />
     <Checkbox id="show" value="show" content="Mostrar Eventos" />
+    <About />
   </main>
 );
 
---
+++ src/objects/About/index.js
@@ -0,0 +1,3 @@
+export const About = () => (
+  <article className="about">Leonardo Brizolla</article>
+);
