9872e8795a9eb9faeb2c1c21531f6843c58c0be7
leonardobrizolla <leonardobrizollars@gmail.com>
2022-01-14 08:30:38 -0300

    Created ProfileUser fonts and colors CSS

--- src/components/ProfileUser/index.js
+++ src/components/ProfileUser/index.js
@@ -1,16 +1,17 @@
+import './styles.css';
 import AvatarProfile from '../../img/avatar-profile.jpeg';
 
 import { AvatarProvider } from '../../objects/AvatarProfile';
 
 export const ProfileUser = () => (
   <dl className="profile-user">
-    <dd>
+    <dd className="avatar">
       <AvatarProvider
         src={AvatarProfile}
         alt="Avatar profile Leonardo Brizolla"
       />
     </dd>
-    <dt>Leonardo Brizolla</dt>
-    <dd>Desenvolvedor Web</dd>
+    <dt className="title">Leonardo Brizolla</dt>
+    <dd className="description">Desenvolvedor Web</dd>
   </dl>
 );
