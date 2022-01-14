51a956bdcba2ab58567fe0a945f1e9a85fea6d7c
leonardobrizolla <leonardobrizollars@gmail.com>
2022-01-14 11:21:52 -0300

    Created handleActiveAboutRemove function

--- src/objects/IconClose/index.js
+++ src/objects/IconClose/index.js
@@ -1,7 +1,7 @@
 import './styles.css';
 
-export const IconClose = () => (
-  <a href="" className="icon-close">
+export const IconClose = ({ onClick }) => (
+  <a href="#TODO" className="icon-close" onClick={onClick}>
     <span className="content">Fechar</span>
   </a>
 );
