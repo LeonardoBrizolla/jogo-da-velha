99bfc4634eef2788fa89f6c3a2b1353f3e0398c2
leonardobrizolla <leonardobrizollars@gmail.com>
2022-01-18 08:13:40 -0300

    Controle next player

--- src/components/HashtagGame/index.js
+++ src/components/HashtagGame/index.js
@@ -1,38 +1,48 @@
-import "./styles.css";
+import { useState } from 'react';
+import './styles.css';
+import { CardGame } from '../../objects/CardGame';
+import { PlayerGame } from '../../objects/PlayerGame';
 
-import { CardGame } from "../../objects/CardGame";
-import { PlayerGame } from "../../objects/PlayerGame";
+export const HashtagGame = () => {
+  const [nextPlayer, setNextPlayer] = useState('x');
 
-export const HashtagGame = () => (
-  <CardGame>
-    <ul className="hashtag-game">
-      <li className="item">
-        <PlayerGame />
-      </li>
-      <li className="item">
-        <PlayerGame />
-      </li>
-      <li className="item">
-        <PlayerGame />
-      </li>
-      <li className="item">
-        <PlayerGame />
-      </li>
-      <li className="item">
-        <PlayerGame />
-      </li>
-      <li className="item">
-        <PlayerGame />
-      </li>
-      <li className="item">
-        <PlayerGame />
-      </li>
-      <li className="item">
-        <PlayerGame />
-      </li>
-      <li className="item">
-        <PlayerGame />
-      </li>
-    </ul>
-  </CardGame>
-);
+  const handleClick = () => {
+    console.log('Next player: ' + nextPlayer);
+
+    setNextPlayer((oldPlayer) => (oldPlayer === 'x' ? 'o' : 'x'));
+  };
+
+  return (
+    <CardGame>
+      <ul className="hashtag-game" onClick={handleClick}>
+        <li className="item">
+          <PlayerGame />
+        </li>
+        <li className="item">
+          <PlayerGame />
+        </li>
+        <li className="item">
+          <PlayerGame />
+        </li>
+        <li className="item">
+          <PlayerGame />
+        </li>
+        <li className="item">
+          <PlayerGame />
+        </li>
+        <li className="item">
+          <PlayerGame />
+        </li>
+        <li className="item">
+          <PlayerGame />
+        </li>
+        <li className="item">
+          <PlayerGame />
+        </li>
+        <li className="item">
+          <PlayerGame />
+        </li>
+      </ul>
+    </CardGame>
+  );
+};
