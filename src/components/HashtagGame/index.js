29af46c01edb6d784216aaf1713eb9ea4570ab15
LeonardoBrizolla <leonardobrizollars@gmail.com>
2022-01-24 22:10:39 -0300

    Show first state

--- src/components/HashtagGame/index.js
+++ src/components/HashtagGame/index.js
@@ -5,10 +5,10 @@ import { PlayerGame } from '../../objects/PlayerGame';
 
 export const HashtagGame = ({ callback }) => {
   const [nextPlayer, setNextPlayer] = useState('x');
-  const [round, setRound] = useState(0);
+  const [lastRound, setLastRound] = useState(1);
   const [history, setHistory] = useState([
     {
-      roude: 0,
+      rounde: 0,
       state: [
         { id: 1, content: '' },
         { id: 2, content: '' },
@@ -22,7 +22,7 @@ export const HashtagGame = ({ callback }) => {
       ],
     },
     {
-      roude: 1,
+      rounde: 1,
       state: [
         { id: 1, content: 'x' },
         { id: 2, content: '' },
@@ -36,34 +36,21 @@ export const HashtagGame = ({ callback }) => {
       ],
     },
   ]);
-  const [players, setPlayers] = useState([
-    { id: 1, content: '' },
-    { id: 2, content: '' },
-    { id: 3, content: '' },
-    { id: 4, content: '' },
-    { id: 5, content: '' },
-    { id: 6, content: '' },
-    { id: 7, content: '' },
-    { id: 8, content: '' },
-    { id: 9, content: '' },
-  ]);
 
   const handleClick = (id) => {
-    setPlayers((oldPlayers) =>
-      oldPlayers.map((oldPlayer) =>
-        oldPlayer.id === id ? { id, content: nextPlayer } : oldPlayer
-      )
-    );
-
+    // setPlayers((oldPlayers) =>
+    //   oldPlayers.map((oldPlayer) =>
+    //     oldPlayer.id === id ? { id, content: nextPlayer } : oldPlayer
+    //   )
+    // );
     callback(nextPlayer);
-
     setNextPlayer((oldPlayer) => (oldPlayer === 'x' ? 'o' : 'x'));
   };
 
   return (
     <CardGame>
       <ul className="hashtag-game">
-        {players.map(({ id, content }) => (
+        {history[lastRound].state.map(({ id, content }) => (
           <li
             key={id}
             className="item"
