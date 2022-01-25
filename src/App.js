e482bf05bfa67624a7c58da5eb6115ebab003867
LeonardoBrizolla <leonardobrizollars@gmail.com>
2022-01-25 00:46:10 -0300

    Mover childrens para o Wrapper

--- src/App.js
+++ src/App.js
@@ -1,44 +1,23 @@
 import { useState } from 'react';
-import './App.css';
 
-import { LayerDark } from './objects/LayerDark';
-import { InputCheckbox } from './objects/InputCheckbox';
 import { HashtagHistoryWrapper } from './objects/HashtagHistoryWrapper';
-import { HistoryGame } from './components/HistoryGame';
+import { LayerDark } from './objects/LayerDark';
 import { HeaderGame } from './components/HeaderGame';
-import { HashtagGame } from './components/HashtagGame';
 import { HeaderInternal } from './components/HeaderInternal';
 import { ProfileUser } from './components/ProfileUser';
 
+import './App.css';
+
 export const App = () => {
   const [activeAbout, setActiveAbout] = useState('');
-  const [history, setHistory] = useState(['Start']);
-  const [isActiveHistory, setIsActiveHistory] = useState(false);
 
   const handleClickAdd = () => setActiveAbout('-active');
   const handleCLickRemove = () => setActiveAbout('');
 
-  const handleClickAddHistory = (player) => {
-    setHistory((state) => [...state, `Adicionou ${player.toUpperCase()}`]);
-  };
-
-  const showHideHistory = () => setIsActiveHistory((oldState) => !!!oldState);
-
-  const changeHistory = (key) => setHistory((old) => old.splice(0, key + 1));
-
   return (
     <main id="main" className="app">
       <HeaderGame onClick={handleClickAdd} />
-      <HashtagHistoryWrapper isActiveHistory={isActiveHistory}>
-        <HashtagGame callback={handleClickAddHistory} />
-        <InputCheckbox
-          onClick={showHideHistory}
-          id="show"
-          value="show"
-          content="Mostrar Eventos"
-        />
-        <HistoryGame history={history} onClick={changeHistory} />
-      </HashtagHistoryWrapper>
+      <HashtagHistoryWrapper />
       <LayerDark className={activeAbout}>
         <HeaderInternal onClick={handleCLickRemove} />
         <ProfileUser />
--- src/objects/HashtagHistoryWrapper/index.js
+++ src/objects/HashtagHistoryWrapper/index.js
@@ -1,8 +1,31 @@
-export const HashtagHistoryWrapper = ({
-  children,
-  isActiveHistory = false,
-}) => (
-  <div className={`hashtagHistory-wrapper ${isActiveHistory ? '-active' : ''}`}>
-    {children}
-  </div>
-);
+import { useState } from 'react';
+
+import { HashtagGame } from '../../components/HashtagGame';
+import { InputCheckbox } from '../../objects/InputCheckbox';
+import { HistoryGame } from '../../components/HistoryGame';
+
+export const HashtagHistoryWrapper = () => {
+  const [history, setHistory] = useState(['Start']);
+  const [isActiveHistory, setIsActiveHistory] = useState(false);
+
+  const handleClickAddHistory = (player) => {
+    setHistory((state) => [...state, `Adicionou ${player.toUpperCase()}`]);
+  };
+  const showHideHistory = () => setIsActiveHistory((oldState) => !!!oldState);
+  const changeHistory = (key) => setHistory((old) => old.splice(0, key + 1));
+
+  return (
+    <div
+      className={`hashtagHistory-wrapper ${isActiveHistory ? '-active' : ''}`}
+    >
+      <HashtagGame callback={handleClickAddHistory} />
+      <InputCheckbox
+        onClick={showHideHistory}
+        id="show"
+        value="show"
+        content="Mostrar Eventos"
+      />
+      <HistoryGame history={history} onClick={changeHistory} />
+    </div>
+  );
+};
