71136c64b1f5e5b7f76a7bb4888c0a05efedaeac
LeonardoBrizolla <leonardobrizollars@gmail.com>
2022-01-24 21:49:02 -0300

    Prepering to save history

--- src/components/HashtagGame/index.js
+++ src/components/HashtagGame/index.js
@@ -5,6 +5,37 @@ import { PlayerGame } from '../../objects/PlayerGame';
 
 export const HashtagGame = ({ callback }) => {
   const [nextPlayer, setNextPlayer] = useState('x');
+  const [round, setRound] = useState(0);
+  const [history, setHistory] = useState([
+    {
+      roude: 0,
+      state: [
+        { id: 1, content: '' },
+        { id: 2, content: '' },
+        { id: 3, content: '' },
+        { id: 4, content: '' },
+        { id: 5, content: '' },
+        { id: 6, content: '' },
+        { id: 7, content: '' },
+        { id: 8, content: '' },
+        { id: 9, content: '' },
+      ],
+    },
+    {
+      roude: 1,
+      state: [
+        { id: 1, content: 'x' },
+        { id: 2, content: '' },
+        { id: 3, content: '' },
+        { id: 4, content: '' },
+        { id: 5, content: '' },
+        { id: 6, content: '' },
+        { id: 7, content: '' },
+        { id: 8, content: '' },
+        { id: 9, content: '' },
+      ],
+    },
+  ]);
   const [players, setPlayers] = useState([
     { id: 1, content: '' },
     { id: 2, content: '' },
