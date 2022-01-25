ae864685670cc0e5fa8bd6c282d4d551b5ae3e2a
LeonardoBrizolla <leonardobrizollars@gmail.com>
2022-01-25 01:19:55 -0300

    Atualizar HashtagGame

--- src/objects/HashtagHistoryWrapper/index.js
+++ src/objects/HashtagHistoryWrapper/index.js
@@ -8,17 +8,58 @@ export const HashtagHistoryWrapper = () => {
   const [history, setHistory] = useState(['Start']);
   const [isActiveHistory, setIsActiveHistory] = useState(false);
 
+  const [nextPlayer, setNextPlayer] = useState('x');
+  const [lastRound, setLastRound] = useState(0);
+  const [historyGame, setHistoryGame] = useState([
+    {
+      round: 0,
+      state: [
+        { id: 1, content: '' },
+        { id: 2, content: '' },
+        { id: 3, content: '' },
+        { id: 4, content: '' },
+        { id: 5, content: '' },
+        { id: 6, content: '' },
+        { id: 7, content: '' },
+        { id: 8, content: '' },
+        { id: 9, content: '' },
+      ],
+    },
+  ]);
+
   const handleClickAddHistory = (player) => {
     setHistory((state) => [...state, `Adicionou ${player.toUpperCase()}`]);
   };
   const showHideHistory = () => setIsActiveHistory((oldState) => !!!oldState);
-  const changeHistory = (key) => setHistory((old) => old.splice(0, key + 1));
+  const changeHistory = (key) => {
+    setLastRound(key);
+    setHistory((old) => old.splice(0, key + 1));
+    setHistoryGame((old) => old.splice(0, key + 1));
+  };
+  const handleClick = (id) => {
+    setHistoryGame((old) => [
+      ...old,
+      {
+        round: lastRound + 1,
+        state: historyGame[lastRound].state.map((player) =>
+          player.id === id ? { id, content: nextPlayer } : player
+        ),
+      },
+    ]);
+    setLastRound((old) => old + 1);
+    handleClickAddHistory(nextPlayer);
+    setNextPlayer((oldPlayer) => (oldPlayer === 'x' ? 'o' : 'x'));
+  };
 
   return (
     <div
       className={`hashtagHistory-wrapper ${isActiveHistory ? '-active' : ''}`}
     >
-      <HashtagGame callback={handleClickAddHistory} />
+      <HashtagGame
+        lastRound={lastRound}
+        onClick={handleClick}
+        historyGame={historyGame}
+      />
       <InputCheckbox
         onClick={showHideHistory}
         id="show"
